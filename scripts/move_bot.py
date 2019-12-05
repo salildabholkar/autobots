60534ee7953383c09f92f8c1c5f8e58d7531ea31
Salil <salil2212@hotmail.com>
2019-12-05 12:12:08 -0500

    Basic movement done

---
+++ scripts/move_bot.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+# Original Code: https://github.com/markwsilliman/turtlebot/blob/master/goforward_and_avoid_obstacle.py
+# TurtleBot must have minimal.launch & amcl_demo.launch running prior to starting this script.
+
+import rospy
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import actionlib
+from actionlib_msgs.msg import GoalStatus
+from geometry_msgs.msg import Pose, Point, Quaternion, PoseWithCovarianceStamped
+from gazebo_msgs.srv import DeleteModel
+from gazebo_msgs.msg import ModelStates
+from nav_msgs.msg import Odometry
+import numpy as np
+import math
+from objects import all_objects
+
+
+class Move():
+    def __init__(self):
+        rospy.init_node('move_bot')
+
+        self.reached = False
+
+        # tell the action client that we want to spin a thread by default
+        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+        rospy.loginfo("wait for the action server to come up")
+
+        # allow up to 5 seconds for the action server to come up
+        self.move_base.wait_for_server(rospy.Duration(5))
+        self.next = ''
+
+        self.all_objects = all_objects
+
+        # Robot pose
+        self.robot_pose = Point(0, 0, 0)
+        positions = [x[1] for x in self.all_objects]
+        self.distances = [math.sqrt((point.x - self.robot_pose.x) ** 2 + (point.y - self.robot_pose.y) ** 2 + (
+                point.z - self.robot_pose.z) ** 2) for point in positions]
+
+        # what to do if shut down (e.g. ctrl + C or failure)
+        rospy.on_shutdown(self.shutdown)
+
+        rospy.loginfo("init move node")
+        self.update_pose()
+
+    # For deleting models from the environment
+    @staticmethod
+    def del_model(model_name):
+        del_model_prox = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)  # Handle to model spawner
+        rospy.wait_for_service('gazebo/delete_model')  # Wait for the model loader to be ready
+        del_model_prox(model_name)  # Remove from Gazebo
+
+    def update_pose(self):
+        self.robot_pose = rospy.wait_for_message('/amcl_pose', PoseWithCovarianceStamped).pose.pose.position
+
+        nearest, dist, elems = self.get_nearest_object_and_index()
+
+        name = self.all_objects[nearest][0]
+        id = self.all_objects[nearest][2]
+
+        if not name.startswith('pickable'):
+            self.all_objects.pop(nearest)
+            self.reached = False
+
+        elif dist < 1:
+            rospy.loginfo('Picked up: ' + name)
+            self.all_objects.pop(nearest)
+            self.del_model(id)
+            if elems == 1:
+                rospy.loginfo('done')
+                rospy.signal_shutdown('done')
+
+        else:
+            rospy.loginfo('Going to pick: ' + name + ' ' + str(dist))
+            self.goto(self.all_objects[nearest][1])
+
+
+    def get_nearest_object_and_index(self):
+        positions = [x[1] for x in self.all_objects]
+        dist = [math.sqrt((point.x - self.robot_pose.x) ** 2 + (point.y - self.robot_pose.y) ** 2 + (point.z - self.robot_pose.z) ** 2) for point in positions]
+        print dist
+        mini = np.argmin(dist)
+        return mini, dist[mini], len(dist)
+
+    def goto(self, goal_pose):
+        self.reached = True
+        goal = MoveBaseGoal()
+        goal.target_pose.pose = rospy.wait_for_message('/amcl_pose', PoseWithCovarianceStamped).pose.pose
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position = goal_pose
+
+        # Start moving
+        print 'sent goal signal'
+        self.move_base.send_goal(goal)
+
+        # Allow TurtleBot up to 200 seconds to complete task
+        success = self.move_base.wait_for_result(rospy.Duration(30))
+
+        state = self.move_base.get_state()
+
+        if success or state == GoalStatus.SUCCEEDED:
+            # We made it!
+            rospy.loginfo("--------Reached, continue to next----------")
+            self.reached = False
+        else:
+            self.move_base.cancel_goal()
+            rospy.loginfo("The base failed to reach after 200s")
+
+    def shutdown(self):
+        rospy.loginfo("Stop")
+        self.move_base.cancel_goal()
+
+    def run(self):
+        r = rospy.Rate(100)  # 100hz
+        while not rospy.is_shutdown():
+            if not self.reached:
+                self.update_pose()
+            r.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        Move().run()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Exception thrown")
---
+++ scripts/objects.py
@@ -0,0 +1,50 @@
+from geometry_msgs.msg import Point
+
+all_objects = [
+    [
+        'pickable Pulley',
+        Point(-0.3, 2.47, 0),
+        'pickable_pulley_part_0'
+    ],
+    [
+        'pickable Fire Hydrant',
+        Point(1.07, 7.22, 0),
+        'pickable_fire_hydrant'
+    ],
+    [
+        'pickable Fire Hydrant',
+        Point(6.19, 6.21, 0),
+        'pickable_fire_hydrant_1'
+    ],
+    [
+        'pickable Fire Hydrant',
+        Point(3.57, 0.58, 0),
+        'pickable_fire_hydrant_0'
+    ],
+    [
+        'Red bin',
+        Point(-0.674, 1.33, 0),
+        'first_2015_trash_can_1'
+    ],
+    [
+        'pickable Car Wheel',
+        Point(1.79, 4.52, 0),
+        'pickable_car_wheel'
+
+    ],
+    [
+        'pickable Car Wheel',
+        Point(4.3, 6.1, 0),
+        'pickable_car_wheel_clone'
+    ],
+    [
+        'Man',
+        Point(-0.8, 4.74, 0),
+        'person_standing_0'
+    ],
+    [
+        'Black Bin',
+        Point(2.32, 3, 0),
+        'first_2015_trash_can_3_4'
+    ],
+]
